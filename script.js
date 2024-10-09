d962badf5967eabb7762353951be4bd174c421f1
rayenbenammar07 <rayenaskri01@gmail.com>
2024-10-09 15:49:29 +0100

    checkpoint-dom2

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const colorBox = document.getElementById("color-box");
-  const changeColorBtn = document.getElementById("change-color-btn");
+  let colorBox = document.getElementById("color-box");
+  let changeColorBtn = document.getElementById("change-color-btn");
 
   function getRandomColor() {
-    const letters = "0123456789ABCDEF";
+    let letters = "0123456789ABCDEF";
     let color = "#";
     for (let i = 0; i < 6; i++) {
       color += letters[Math.floor(Math.random() * 16)];
     }
     return color;
   }
-
 
   changeColorBtn.addEventListener("click", function () {
     colorBox.style.backgroundColor = getRandomColor();
